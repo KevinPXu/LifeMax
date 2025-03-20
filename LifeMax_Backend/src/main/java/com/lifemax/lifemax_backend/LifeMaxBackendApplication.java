2132dbcc6d4819cda59854ceeb9ad9ec96fb24c1
kevin <xu.kevin2@northeastern.edu>
2025-03-20 15:14:54 -0400

    init spring/maven project

---
+++ LifeMax_Backend/src/main/java/com/lifemax/lifemax_backend/LifeMaxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lifemax.lifemax_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeMaxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeMaxBackendApplication.class, args);
+    }
+
+}
---
+++ LifeMax_Backend/src/test/java/com/lifemax/lifemax_backend/LifeMaxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifemax.lifemax_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeMaxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
